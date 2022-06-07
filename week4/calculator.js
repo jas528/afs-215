4bf15b6e0163bfc826f87b5d3fbc610cb3c61904
jas528 <76809910+jas528@users.noreply.github.com>
2022-06-06 23:18:14 -0500

    wk4

---
+++ week4/calculator.js
@@ -0,0 +1,30 @@
+//Create one failing test,one passing test XX
+//use TDD process create a function that act as a calculator for addition, subtraction, division, and multiplication.
+//Finally, an error message should be returned if the argument provided is not valid.
+// it('Expect added to equal 4', function() {
+// expect(2 + 2).has.to.equal(4)
+// })
+
+// how to set up function line 14
+function myCalculator(arg, num1, num2) {
+  //my calucalator function
+  if (arg === "add") {
+    // adding = ()=> {
+    return num1 + num2; // return this.num11 + this num 11
+  }
+
+  if (arg === "subtract") {
+    // subtract = ()=> {
+    return num1 - num2; // return this.num 88 - this num 44
+  }
+
+  if (arg === "division") {
+    return num1 / num2;
+  }
+
+  if (arg === "multiplication") {
+    return num1 * num2;
+    // may not need else statement
+  }
+}
+module.exports= myCalculator
---
+++ week4/testingcalculator.js
@@ -0,0 +1,15 @@
+const calculator=require("./calculator") 
+const expect = require("chai").expect;
+
+it("calling calculator ", function () {
+//  true equal to false can count as a failing or passing result.
+calculator()
+  });
+//   it("expect passing test", "att", function () {
+//     expect(myCalculator("")).to.equal("");
+//     return "";
+//   });
+  
+//   it("return error message when not passed valid", function () {
+//     expect(myCalculator("")).to.equal("");
+//   });
